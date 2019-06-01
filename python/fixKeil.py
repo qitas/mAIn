794d73a2b5f1e22d12b0709f9ae1d0ca8096847b
qitas <qitas@hotmail.com>
2019-06-01 22:12:43 +0800

    fix python shell

--- python/fixKeil.py
+++ python/fixKeil.py
@@ -4,15 +4,24 @@ import re
 import sys
 
 try:
-    fin = open(sys.argv[1], 'r', encoding="utf-8")
+    fin = open(sys.argv[1], 'r+', encoding="utf-8")
 except IOError:
     e = "Can't open output file for writing: " + sys.argv[1]
     print( __file__, ":", e)
     sys.exit()
 
-for ln in fin.readlines():
-    obj = re.search(r'\\Output\\',ln)
-    if obj :
+#text = "JGood is a handsome boy, he is cool, clever, and so on..."
+#print(re.sub(r'\s+', '-', text))
+
+for line in fin.readlines():
+    obj = re.search(r'\\Output\\',line)
+    #obj = re.sub(r'\\Output\\',r'\\obj\\',ln)
+    #print(obj)
+    #obj.replace('\\Output\\', '\\obj\\')
+    if obj:
+        print(line)
+        buf = line.replace('\\Output\\', '\\obj\\')
+        print(buf)
         print("fix Output")
   
 fin.close
