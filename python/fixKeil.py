e70f53dc4f5317910969b1c7218a0f32798da4ad
qitas <qitas@hotmail.com>
2019-06-01 23:49:59 +0800

    fix not finish

--- python/fixKeil.py
+++ python/fixKeil.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 import re
+import os
 import sys
+import argparse
+import fileinput
+
+parse=argparse.ArgumentParser()
+parse.add_argument('-p','--path')
+args=parse.parse_args()
+path=args.path
 
 try:
     fin = open(sys.argv[1], 'r', encoding="utf-8")
@@ -11,8 +19,13 @@ except IOError:
     print( __file__, ":", e)
     sys.exit()
 
+print(os.listdir())
 #text = "JGood is a handsome boy, he is cool, clever, and so on..."
 #print(re.sub(r'\s+', '-', text))
+for maindir, subdir, file_name_list in os.walk(path):
+    print("1:",maindir) #当前主目录
+    print("2:",subdir) #当前主目录下的所有目录
+    print("3:",file_name_list)  #当前主目录下的所有文件
 
 for line in fin.readlines():
     rall=re.compile(r'.*')
@@ -39,6 +52,6 @@ for line in fin.readlines():
 
 fin.close
 
-
+#os.remove()
     
     
