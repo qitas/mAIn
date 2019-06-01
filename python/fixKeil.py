dfa2878c04eba68c96cc56c04dbc260e4d151b06
qitas <qitas@hotmail.com>
2019-06-01 21:49:35 +0800

    add python shell

--- python/fixKeil.py
+++ python/fixKeil.py
@@ -4,19 +4,18 @@ import re
 import sys
 
 try:
-    f = open(sys.argv[1], 'r', encoding="utf-8")
+    fin = open(sys.argv[1], 'r', encoding="utf-8")
 except IOError:
     e = "Can't open output file for writing: " + sys.argv[1]
     print( __file__, ":", e)
     sys.exit()
 
-for ln in f.readlines():
-    obj = re.search("(\\\\Output\\\\)",ln)
+for ln in fin.readlines():
+    obj = re.search(r'\\Output\\',ln)
     if obj :
-        print("find Output")
-    
-f.close
-
+        print("fix Output")
+  
+fin.close
 
 
     
