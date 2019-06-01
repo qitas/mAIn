faf5ffd25fdce13376d7ab227c3599600de921d7
qitas <qitas@hotmail.com>
2019-06-01 23:58:19 +0800

    fix not finish

--- python/fixKeil.py
+++ python/fixKeil.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
 
 import re
 import os
@@ -6,10 +6,22 @@ import sys
 import argparse
 import fileinput
 
+'''
 parse=argparse.ArgumentParser()
 parse.add_argument('-p','--path')
 args=parse.parse_args()
 path=args.path
+
+for maindir, subdir, file_name_list in os.walk(path):
+    print("1:",maindir) #当前主目录
+    print("2:",subdir) #当前主目录下的所有目录
+    print("3:",file_name_list)  #当前主目录下的所有文件
+'''
+
+dirs=os.listdir()
+for dir in dirs: 
+    if dir: 
+        print(dir)
 
 try:
     fin = open(sys.argv[1], 'r', encoding="utf-8")
@@ -18,15 +30,8 @@ except IOError:
     e = "Can't open output file for writing: " + sys.argv[1]
     print( __file__, ":", e)
     sys.exit()
-
-print(os.listdir())
 #text = "JGood is a handsome boy, he is cool, clever, and so on..."
 #print(re.sub(r'\s+', '-', text))
-for maindir, subdir, file_name_list in os.walk(path):
-    print("1:",maindir) #当前主目录
-    print("2:",subdir) #当前主目录下的所有目录
-    print("3:",file_name_list)  #当前主目录下的所有文件
-
 for line in fin.readlines():
     rall=re.compile(r'.*')
     #obj = re.search(r'\\Output\\',line)
