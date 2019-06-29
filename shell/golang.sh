cee608dc5ec14f13526cdb6c32d61160a3b6174c
qitas <qitas@qitas.cn>
2019-06-29 14:12:00 +0800

    fix golang

--- shell/golang.sh
+++ shell/golang.sh
@@ -5,17 +5,18 @@ WorkPath=`pwd`
 str="go :  "
 ret=`whereis go`
 if [ ${#ret} -lt ${#str} ]; then
-	cd $WorkPath
 	if [ ! -f $WorkPath/golang.tar.gz ]; then
 		cd $WorkPath
 		wget -O golang.tar.gz https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
 	fi
-	if [ ! -d /usr/local/go ]; then
-		sudo tar -C /usr/local -xzf golang.tar.gz
+	if [ ! -d $HOME/golang ]; then
+		mkdir $HOME/golang
+		tar -C $HOME/golang -xzf golang.tar.gz
 	fi
 	
-	echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
-	echo 'export GOPATH='$HOME'/go' >> ~/.bashrc
+	echo 'export GOROOT='$HOME'/golang/root' >> ~/.bashrc
+	mkdir $HOME/golang/root
+	echo 'export GOPATH='$HOME'/golang/go' >> ~/.bashrc
 	echo 'export PATH=$PATH:$GOROOT/bin:$GOPATH/bin' >> ~/.bashrc
 	source ~/.bashrc
 	echo -e "export golang path\n${Line}"  
